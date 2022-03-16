b7e98ea55b9e5e1a5d014acb6fd4765d729d5635
jordan <jharrison1@qa.com>
2022-03-16 15:30:49 +0000

    Added fancy request validation

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const bloop = (req, res, next) => {
     return next();
 }
 
+const checkMethod = (methods = []) => (req, res, next) => {
+    if (!methods.includes(req.method))
+        return next({status: 405, message: `Incorrect request method: ${req.method}\nShould be: ${methods}`})
+    else 
+        return next();
+}
+
+const checkContentType = (type = "application/json") => (req, res, next) => {
+    if (!req.is(type))
+        return next({status: 415, message: `Invalid data format. Please send your data in the format: ${type}`});
+    else 
+        return next();
+}
+
 
 app.use((err, req, res, next) => { // never called as there's been no error at this point
     console.log("bloop");
@@ -27,13 +41,13 @@ app.use((req, res, next) => {
     return next(); // next -> app.post(...)
 });
 
-app.get("/", (request, response) => {
+app.use("/", checkMethod("GET"), (request, response) => {
     response.send(cows.speak("Mooooooo"));
 });
 
-app.get("/getAll", (req, res) => res.json(data));
+app.use("/getAll", checkMethod("GET"), (req, res) => res.json(data));
 
-app.get("/get/:id", (req, res, next) => {
+app.use("/get/:id", checkMethod("GET"), (req, res, next) => {
     const id = Number.parseInt(req.params.id);
 
     if (id == null || undefined || id === NaN) 
@@ -44,22 +58,30 @@ app.get("/get/:id", (req, res, next) => {
     res.json(data[id]);
 })
 
-app.post("/create", bloop, (req, res) => {
+app.use("/create", checkMethod("POST"), checkContentType(), bloop, (req, res) => {
     const person = req.body;
     data.push(person);
     res.status(201).send("Successfully created");
 });
 
-app.put("/replace/:id", (req, res) => {
+app.use("/replace/:id", checkMethod("PUT"), (req, res) => {
     const newPerson = req.query;
-    const id = req.params.id;
+    const id = Number.parseInt(req.params.id);
+
+    if (id == null || undefined || id === NaN) 
+        return next({ status: 400, message: "Invalid id" });
+    else if (id > data.length) 
+        return next({ status: 404, message: "No person found with id " + id });
     
     data.splice(id, 1, newPerson);
     res.status(202).json(data[id]);
 });
 
-app.delete("/remove/:id", (req, res) => {
-    const id = req.params.id;
+app.use("/remove/:id", checkMethod("DELETE"), (req, res) => {
+    const id = Number.parseInt(req.params.id);
+
+    if (id == null || undefined || id === NaN) 
+        return next({ status: 400, message: "Invalid id" });
 
     data.splice(id, 1);
     res.sendStatus(204);
@@ -69,13 +91,13 @@ app.use("*", (req, res, next) => {
     return next({status: 404, message: "Invalid URL"});
 });
 
-// never gonna get hit due to response being sent
+// never gonna get hit due to response being sent previously
 app.use((req, res, next) => {
     console.log("Never gonna give you up");
 })
 
 app.use((err, req, res, next) => {
-     res.status(err.status).send(err.message);
+     res.status(err.status || 400).send(err.message || "ERROR");
 })
 
 
