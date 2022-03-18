5b31b4a6350c4f94e5d0a5093aa6bef507c43976
jordan <jharrison1@qa.com>
2022-03-18 11:25:12 +0000

    added getByID example

--- tests/person_test.js
+++ tests/person_test.js
@@ -7,7 +7,29 @@ chai.use(chaiHttp); // adding the plugin to the library
 
 const server = require("../index");
 
+const Person = require("../db");
+
 describe("Test Person", () => {
+
+    let testPerson;
+
+    beforeEach((done) => {
+        Person.deleteMany((err) => {
+            if (!err) {
+                Person.create({
+                    name: "Bob",
+                    age: 40,
+                    jobTitle: "Builder"
+                }, (err, created) => {
+                    if (!err) {
+                        testPerson = created;
+                        return done();
+                    }
+                })
+            }
+        })
+    })
+
     it("Should create a person", (done) => {
         chai.request(server).post("/person/create").send({
             name: "Jordan",
@@ -27,6 +49,17 @@ describe("Test Person", () => {
             expect(res).to.have.status(400);
             expect(res).to.haveOwnProperty("text", "Person validation failed: jobTitle: Path `jobTitle` is required., age: Path `age` is required., name: Path `name` is required.")
             return done();
+        });
+    });
+
+    it("Should find a Person", (done) => {
+        chai.request(server).get("/person/get/" + testPerson.id).end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.haveOwnProperty("name", "Bob");
+            expect(res.body).to.haveOwnProperty("age", 40);
+            expect(res.body).to.haveOwnProperty("jobTitle", "Builder");
+            return done();
         })
     })
-})
+});
